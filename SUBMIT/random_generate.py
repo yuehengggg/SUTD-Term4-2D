439674ced4251e7ccf79b1678b603bfd1c7c0d38
yuehengggg <97967305+yuehengggg@users.noreply.github.com>
2022-03-16 20:25:08 +0800

    Update random_generate.py

--- SUBMIT/random_generate.py
+++ SUBMIT/random_generate.py
@@ -4,13 +4,12 @@
 import random
 import copy
 
-def random_generate():
-    '''
-    Randomly generate test cases
+import random
+import copy
 
-    '''
-    v=random.randint(1,500)
-    c=random.randint(1,500)
+def random_generate():
+    v=random.randint(2,500)
+    c=random.randint(v//2+1,500)
     return randomgenerate(v,c)
 
 
@@ -18,22 +17,40 @@ def randomgenerate(variable,clause):
     output=[]
     output.append(variable)
     output.append(clause)
-    cnf=[]
+    empty=[]
+    cnf=[list(empty)for i in range(0,clause)]
     inp=list(range(1,variable+1))
-    for i in range(0,clause):
-        c=[]
+    indexc=list(range(0,clause))
+    
+    for i in inp:
+        cnfindex=random.choice(indexc)
+     
+        sign=random.randint(1,2)
+        if sign==1:
+            cnf[cnfindex].append(i)
+        else:
+            cnf[cnfindex].append(-i)
+        if len(cnf[cnfindex])==2:
+            indexc.remove(cnfindex)
+    for i in indexc:
         inpcopy=copy.deepcopy(inp)
-        for p in range(0,2):
+        if len(cnf[i])==1:
+            if cnf[i][0]>0:
+                inpcopy.remove(cnf[i][0])
+            else:
+                inpcopy.remove(-cnf[i][0])
+        for p in range(0,2-len(cnf[i])):
             r=random.randint(1,2)
-            l=len(inpcopy)
-            randindex=random.randint(0,l-1)
+            li=len(inpcopy)
+            randindex=random.randint(0,li-1)
             v=inpcopy[randindex]
+
             if r==1:
-                c.append(v)
+                cnf[i].append(v)
             if r==2:
                
-                c.append(-v)
-            inpcopy.remove(v)        
-        cnf.append(c)
+                cnf[i].append(-v)
+            inpcopy.remove(v)
+
     output.append(cnf)
     return output
